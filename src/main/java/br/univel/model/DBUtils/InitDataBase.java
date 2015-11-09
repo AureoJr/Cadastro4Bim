79109645cb915773803cc66f7566b5eff8f52220
aureo <aureojr1@gmail.com>
2015-11-09 03:22:37 -0200

    cRIANDO O BANCO

--- src/main/java/br/univel/model/DBUtils/InitDataBase.java
+++ src/main/java/br/univel/model/DBUtils/InitDataBase.java
@@ -1,6 +1,11 @@
 package br.univel.model.DBUtils;
 
+import java.io.File;
 import java.lang.reflect.ReflectPermission;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * 
@@ -12,13 +17,43 @@ import java.lang.reflect.ReflectPermission;
  */
 public class InitDataBase {
 
+	public static String pacote = "br.unive.model"; 
+	
 	public static void main(String[] args) {
 		
 
 	}
 
-	public void criarbanco(){
-
+	/**
+	 * Cria todas as tabelas referencciadas no model
+	 * @throws ClassNotFoundException 
+	 */
+	public void criarbanco() throws ClassNotFoundException{
+		// Lista para armazenar as Classes
+		List<Class<?>> classes = new ArrayList<>();
+		
+		//Sufixo para definir os arquivos .class
+		String suffix = ".class";
+		
+		// Criando uma URL para adiquirir o arquivo(pasta) aprtir do pacote
+		URL packageUrl = Thread.currentThread().getContextClassLoader().getResource(InitDataBase.pacote);
+		
+		// Se o arquivo não for encontrado Fudeu Bahia 
+		if(packageUrl == null){
+			throw new IllegalArgumentException("Pacote "+InitDataBase.pacote+" não existe" );
+		}
+		
+		File pacote = new File(InitDataBase.pacote);
+		
+		for (File classe : pacote.listFiles()) {
+			if(classe.getName().endsWith(suffix)){
+				classes.add(Class.forName(classe.getName()));
+			}
+		}
+		
+		// cONTINUA NO PROXIMO EPISÓDO
+				
+		
 	}
 	
 }
