5e212c5e22f952babb326368bf765310acaa170a
aureo <aureojr1@gmail.com>
2015-11-19 22:02:10 -0200

    Valor default para Id

--- src/main/java/br/univel/model/DBUtils/annotations/Id.java
+++ src/main/java/br/univel/model/DBUtils/annotations/Id.java
@@ -16,8 +16,7 @@ import java.lang.annotation.Target;
  */
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
 public @interface Id {
 
-	public String nome() default ""; 
+	public String nome() default "ID"; 
 }
