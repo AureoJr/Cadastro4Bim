0b6a1aa7f9931ccadf24c438a30317275ca6ecd8
anxsjunior <aureojr1@gmail.com>
2015-11-30 22:42:24 -0200

    Criação fdo miolo

--- src/main/java/br/univel/view/MioloCadastroCliente.java
+++ src/main/java/br/univel/view/MioloCadastroCliente.java
@@ -5,11 +5,9 @@ import javax.swing.JLabel;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class MioloCadastroCliente extends JPanel {
-	
-	protected JTextField txfId;
-	protected JTextField txfNome;
              
 	/**      
 	 * Create the panel.
@@ -22,38 +20,12 @@ public class MioloCadastroCliente extends JPanel {
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
-		JLabel lblId = new JLabel("Id");
-		GridBagConstraints gbc_lblId = new GridBagConstraints();
-		gbc_lblId.anchor = GridBagConstraints.EAST;
-		gbc_lblId.insets = new Insets(0, 0, 5, 5);
-		gbc_lblId.gridx = 0;
-		gbc_lblId.gridy = 0;
-		add(lblId, gbc_lblId);
-		
-		txfId = new JTextField();
-		GridBagConstraints gbc_txfId = new GridBagConstraints();
-		gbc_txfId.insets = new Insets(0, 0, 5, 0);
-		gbc_txfId.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txfId.gridx = 1;
-		gbc_txfId.gridy = 0;
-		add(txfId, gbc_txfId);
-		txfId.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Nome");
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
-		gbc_lblNewLabel.gridx = 0;
-		gbc_lblNewLabel.gridy = 1;
-		add(lblNewLabel, gbc_lblNewLabel);
-		
-		txfNome = new JTextField();
-		GridBagConstraints gbc_txfNome = new GridBagConstraints();
-		gbc_txfNome.fill = GridBagConstraints.HORIZONTAL;
-		gbc_txfNome.gridx = 1;
-		gbc_txfNome.gridy = 1;
-		add(txfNome, gbc_txfNome);
-		txfNome.setColumns(10);
+		JButton btnAddUser = new JButton("");
+		GridBagConstraints gbc_btnAddUser = new GridBagConstraints();
+		gbc_btnAddUser.insets = new Insets(0, 0, 0, 5);
+		gbc_btnAddUser.gridx = 0;
+		gbc_btnAddUser.gridy = 1;
+		add(btnAddUser, gbc_btnAddUser);
 
 	}
 
