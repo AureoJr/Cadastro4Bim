8ee1beb00e87989f298fdeb204ddca64fe404f56
anxsjunior <aureojr1@gmail.com>
2015-12-03 20:55:13 -0200

    Cadastro de cliete

---
+++ src/main/java/br/univel/view/inside/CadastroCliente.java
@@ -0,0 +1,59 @@
+package br.univel.view.inside;
+import javax.swing.JPanel;
+import java.awt.GridBagLayout;
+import javax.swing.JLabel;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+/**
+ * Cadastro de Cliente
+ * 
+ * @author Aureo Junior	
+ * @since 03/12/2015 19:59
+ *
+ */
+public class CadastroCliente extends JPanel {
+             
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JTextField textField;
+
+	/**      
+	 * Create the panel.
+	 */
+	public CadastroCliente() {
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JLabel lblNome = new JLabel("Nome");
+		GridBagConstraints gbc_lblNome = new GridBagConstraints();
+		gbc_lblNome.anchor = GridBagConstraints.EAST;
+		gbc_lblNome.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNome.gridx = 0;
+		gbc_lblNome.gridy = 1;
+		add(lblNome, gbc_lblNome);
+		
+		textField = new JTextField();
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.gridx = 1;
+		gbc_textField.gridy = 1;
+		add(textField, gbc_textField);
+		textField.setColumns(10);
+
+	}
+	
+	@Override
+	public String toString() {
+		return "Cadastro de Cliente"; 
+	}
+
+}
